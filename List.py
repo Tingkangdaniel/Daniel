614cdbc8d7699db9b0962233085a42d6e98dc16d
Tingkangdaniel <dannytkj7@gmail.com>
2020-01-16 16:08:53 +0630

    py

---
+++ List.py
@@ -0,0 +1,57 @@
+word = 'Programming'
+word[0]
+word[-2]
+word[3:5]
+word[4:9]
+word[:5]
+word[8:]
+word[5:-3]
+word[:2] + word[3:]
+
+len(word)
+
+#len = length
+
+Square = 'Square'
+len(Square)
+len(word) + len(Square)
+
+cube = [10, 20, 30, 45, 50]
+cube
+cube[3] = 40
+cube
+cube[5] = 60 #error out of range
+cube.append(60)
+cube.append(4+10+20+36)
+cube
+
+programmA = ['A','B','C','D','E']
+programmB = ['a','b','c','d','e']
+programmC = programmA + programmB
+programmC
+['A','B','C','D','E','a','b','c','d','e']
+
+programD = [1, 2, 3, 4, 5]
+programmC =programmC + programD
+programmC[9:] = []
+programmC[5:9] = programD
+programmC
+
+len(programmC)
+
+a = [10,20,30,40,50]
+b = [60,70,80,90,100]
+c = [110,120,130,140,150]
+x = [a, b, c]
+x
+
+x[0][]
+x[][3]
+x[1][2]
+x[][]
+x[0][0]
+x[1][1]
+x[]
+
+x[1][2] + x[2][0]
+x[0][1] - x[2][1]
